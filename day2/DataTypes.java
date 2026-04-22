62935b6cf42981ad236191fd7db14f91034ceed5
PRIYANKA PAMURI <41770146+pamuripriyanka@users.noreply.github.com>
2026-04-22 20:53:54 +0530

    Create DataTypes.java

---
+++ day2/DataTypes.java
@@ -0,0 +1,42 @@
+package day2;
+
+public class DataTypes {
+
+	public static void main(String[] args) 
+	{
+		int a=100, b= 200;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(a+b);
+
+		byte by=120;
+		System.out.println(by);
+		
+		short sh=250;
+		System.out.println(sh);
+		
+		long l= 129384653673654L; //L-Literal is needed at end of the value.
+		System.out.println(l);
+		
+		
+		//Decimal number -Double, float
+		
+		float price =150.f;
+		System.out.println(price);
+		
+		double item_price = 145.985938;
+		System.out.println(item_price);
+		
+		char grade= 'D';
+		System.out.println(grade);
+		
+		boolean bool=true;
+		System.out.println(bool);
+		
+		String str="Hello";
+		System.out.println(str);
+		
+		
+	}
+
+}
