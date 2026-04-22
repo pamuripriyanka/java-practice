faaa8c6d5a205234d9cb3a23a4de5dfb9c4edfd5
PRIYANKA PAMURI <41770146+pamuripriyanka@users.noreply.github.com>
2026-04-22 21:58:10 +0530

    Create AllExamples.java

---
+++ practice1/AllExamples.java
@@ -0,0 +1,13 @@
+package Practice1;
+
+public class AllExamples 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println(" How to Print in Java!");
+		System.out.printf("%S %n","Hello Coder!");
+		System.out.println(" Guru99!");
+	}
+
+}
